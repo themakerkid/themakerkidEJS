24a92fb59c8e59a7966c84064ae3aeb753183e3a
= <=>
2017-03-03 17:31:45 +0000

    Added first 80 words and more responsive videos.

--- app/snippets/forms.py
+++ app/snippets/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, TextAreaField
+from wtforms import StringField, SubmitField, SelectField, TextAreaField, ValidationError
 from wtforms.validators import Length, DataRequired
 from ..models import Snippet
 
-choices = ((Snippet.HTML, "HTML"), (Snippet.CSS, "CSS"), (Snippet.JAVASCRIPT, "JavaScript"), (Snippet.ARDUINO_C, "Arduino C++"), (Snippet.PYTHON, "Python"), (Snippet.OTHER, "Other"))
+choices = ((0, "Please select a language"), (Snippet.HTML, "HTML"), (Snippet.CSS, "CSS"), (Snippet.JAVASCRIPT, "JavaScript"), (Snippet.ARDUINO_C, "Arduino C++"), (Snippet.PYTHON, "Python"), (Snippet.OTHER, "Other"))
 
 class SnippetForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired(), Length(1, 64, 'Title should be no more than 64 characters')])
@@ -16,7 +16,15 @@ class SnippetForm(FlaskForm):
         super(SnippetForm, self).__init__()
         self.language.choices = choices
 
+    def validate_language(self, field):
+        if field.data == 0:
+            raise ValidationError("You must select a language.")
+
 class CommentForm(FlaskForm):
     body = TextAreaField("Content", validators=[DataRequired()])
     submit = SubmitField("Save", false_values="submit")
     cancel = SubmitField("Cancel", false_values="cancel")
+
+class SearchForm(FlaskForm):
+    search = StringField("Search Query", validators=[DataRequired()])
+    submit = SubmitField("Search")
