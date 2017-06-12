b72a819c921e9517a3c0c2c0163fd759563a6f3a
= <=>
2017-06-12 21:53:21 +0100

    Changed video link on home page.

--- app/projects/forms.py
+++ app/projects/forms.py
@@ -8,7 +8,7 @@ class ProjectForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired(), Length(1, 64, "Title must be under 64 characters")])
     description = TextAreaField("Description (with images)", validators=[DataRequired()])
     vid_url = URLField("Video URL (optional)", validators=[Optional(), Length(1, 100, "Video URL must be less than 100 characters")])
-    parts = TextAreaField("Parts (Each on a new line)")
+    parts = TextAreaField("Parts (Each on a new line)", validators=[DataRequired()])
     steps = TextAreaField("Steps [Leave 3 stars/asterisks (***) between each step]", validators=[DataRequired()])
     code = TextAreaField("The code!", validators=[DataRequired()])
     status = RadioField("", coerce=int, choices=[(0, "Draft"), (1, "Final")], default="0")
